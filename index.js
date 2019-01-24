bcfeff66c9a6d018c977d04661753bff0b846641
Santhosh <santhosh@ilimi.in>
2019-01-24 10:55:41 +0530

    Force commit

--- index.js
+++ index.js
@@ -12,149 +12,329 @@ var questions;
 var responses = [];
 var results = [];
 var packetId;
+var periodId;
+var classContext = {};
 var currentQuestion;
+var topicData = {};
 
 app.use(bodyParser.json());
 
 app.use("/", express.static(__dirname + "/"));
 
+//
+//  Multiplayer Quiz - Start
+//
+
 io.on("connection", socket => {});
 
 io.on("disconnect", function() {
-  io.emit("quiz-ended", { quizId: socket.quizId, event: "end" });
+    io.emit("quiz-ended", { quizId: socket.quizId, event: "end" });
 });
 
 app.get("/startQuiz", (req, res) => {
-  responses = [];
-  questions = JSON.parse(fs.readFileSync("questions.json", "utf-8"));
-  joel = _.find(questions, { identifier: "do_112682561142702080162" });
-  questions = _.shuffle(questions);
-  questions = _.takeRight(questions, 9);
-  questions.unshift(joel);
-  questions = _.shuffle(questions);
-  io.emit("startQuiz", {});
-  res.end();
+    responses = [];
+    questions = JSON.parse(fs.readFileSync("questions.json", "utf-8"));
+    joel = _.find(questions, { identifier: "do_112682561142702080162" });
+    questions = _.shuffle(questions);
+    questions = _.takeRight(questions, 9);
+    questions.unshift(joel);
+    questions = _.shuffle(questions);
+    io.emit("startQuiz", {});
+    res.end();
 });
 
 app.get("/nextQuestion/:id", (req, res) => {
-  var id = parseInt(req.params.id, 10);
-  currentQuestion = questions[id];
-  io.emit("question", { data: currentQuestion });
-  res.end();
+    var id = parseInt(req.params.id, 10);
+    currentQuestion = questions[id];
+    io.emit("question", { data: currentQuestion });
+    res.end();
 });
 
 app.get("/endQuiz", (req, res) => {
-  io.emit("endQuiz");
-  var users = _.map(responses, "user");
-  users = _.uniqWith(users, _.isEqual);
-  results = [];
-  _.forEach(users, function(user) {
-    var userResult = user;
-    userResult.time = 0;
-    userResult.score = 0;
-    _.forEach(responses, function(response) {
-      if (user.code === response.user.code) {
-        userResult.time = userResult.time + response.option.time;
-        if (response.option.isCorrect) {
-          userResult.score++;
-        }
-      }
+    io.emit("endQuiz");
+    var users = _.map(responses, "user");
+    users = _.uniqWith(users, _.isEqual);
+    results = [];
+    _.forEach(users, function(user) {
+        var userResult = user;
+        userResult.time = 0;
+        userResult.score = 0;
+        _.forEach(responses, function(response) {
+            if (user.code === response.user.code) {
+                userResult.time = userResult.time + response.option.time;
+                if (response.option.isCorrect) {
+                    userResult.score++;
+                }
+            }
+        });
+        userResult.score = (userResult.score / 10) * 100;
+        results.push(userResult);
     });
-    userResult.score = (userResult.score / 10) * 100;
-    results.push(userResult);
-  });
-  results = _.orderBy(results, ["score", "time"], ["desc", "asc"]);
-  var i = 1;
-  results.map(r => {
-    return (r.rank = i++);
-  });
-  io.emit("results", { results: results });
-  console.log(results);
-  res.status(200).send({ results: results });
+    results = _.orderBy(results, ["score", "time"], ["desc", "asc"]);
+    var i = 1;
+    results.map(r => {
+        return (r.rank = i++);
+    });
+    io.emit("results", { results: results });
+    console.log(results);
+    res.status(200).send({ results: results });
 });
 
 app.post("/userResponse", (req, res) => {
-  responses.push(req.body);
-  res.end();
+    responses.push(req.body);
+    res.end();
 });
 
-app.post("/packet/:id", function(req, res) {
-  packetId = req.params.id;
-  res.status(200).send({ status: true });
+const request = require("request-promise");
+const getQuestions = async allQuestionIds => {
+    const questions = allQuestionIds.map(async questionId => {
+        const options = {
+            method: "GET",
+            url: `https://dev.sunbirded.org/action/assessment/v3/items/read/${questionId}`,
+            json: true
+        };
+        const response = await request(options);
+        return response.result.assessment_item;
+    });
+    return await Promise.all(questions);
+};
+
+//
+//  Multiplayer Quiz - End
+//
+
+//
+//  Classroom APIs - Start
+//
+
+app.post("/packet/:id/:periodId", function(req, res) {
+    packetId = req.params.id;
+    classContext.periodId = req.params.periodId;
+    var periodData = periodMap[classContext.periodId];
+    classContext.grade = periodData.grade;
+    classContext.subject = periodData.subject;
+    classStudents = _.shuffle(JSON.parse(JSON.stringify(periodData.students)));
+    res.status(200).send({ status: true });
 });
 
-app.get("/packet", function(req, res) {
-  res.status(200).send({ id: packetId });
+
+var periodMap = {
+    "PTCH1_2": {
+        "grade": "Class 3",
+        "subject": "EVS",
+        "students": [
+            {"studentId": "STU1", "studentName": "Nandu"}, 
+            {"studentId": "STU2", "studentName": "Vedanth"},
+            {"studentId": "STU3", "studentName": "Aashna"},
+            {"studentId": "STU4", "studentName": "Akira"},
+            {"studentId": "STU5", "studentName": "Vandhana"}
+        ]
+    },
+    "PTCH2_2": {
+        "grade": "Class 4",
+        "subject": "Geography",
+        "students": [
+            {"studentId": "STU6", "studentName": "Anu"}, 
+            {"studentId": "STU7", "studentName": "Deepa"},
+            {"studentId": "STU8", "studentName": "Deepak"},
+            {"studentId": "STU9", "studentName": "Manoj"},
+            {"studentId": "STU10", "studentName": "Hari"}
+        ]
+    },
+    "PTCH3_2": {
+        "grade": "Class 4",
+        "subject": "EVS",
+        "students": [
+            {"studentId": "STU6", "studentName": "Anu"}, 
+            {"studentId": "STU7", "studentName": "Deepa"},
+            {"studentId": "STU8", "studentName": "Deepak"},
+            {"studentId": "STU9", "studentName": "Manoj"},
+            {"studentId": "STU10", "studentName": "Hari"}
+        ]
+    },
+    "PTCH4_2": {
+        "grade": "Class 5",
+        "subject": "Geography",
+        "students": [
+            {"studentId": "STU11", "studentName": "Pritha"},
+            {"studentId": "STU12", "studentName": "Bindu"},
+            {"studentId": "STU13", "studentName": "Neel"},
+            {"studentId": "STU14", "studentName": "Harsha"},
+            {"studentId": "STU15", "studentName": "Harshita"}
+        ]
+    },
+    "PTCH5_2": {
+        "grade": "Class 8",
+        "subject": "Science",
+        "students": [
+            {"studentId": "STU16", "studentName": "Rajesh"},
+            {"studentId": "STU17", "studentName": "Mohana"},
+            {"studentId": "STU18", "studentName": "Ramya"},
+            {"studentId": "STU19", "studentName": "Banu"},
+            {"studentId": "STU20", "studentName": "Boominathan"}
+        ]
+    }
+}
+
+var classContext = {
+    "stallId": "STA2",
+    "stallName": "Classroom",
+    "ideaId": "IDE6",
+    "ideaName": "APPU Classroom",
+    "school": "QSBB higher secondary school",
+    "district": "Bangalore"
+};
+
+var classStudents = [];
+
+app.post("/classroom/start", function(req, res) {
+
+    classContext.visitorId = req.body.visitorId;
+    classContext.visitorName = req.body.visitorName;
+    var student = classStudents.pop();
+    classContext.studentId = student.studentId;
+    classContext.studentName = student.studentName;
+    postTelemetryEvent({
+        "eid": "DC_ATTENDANCE",
+        "ets": new Date().getTime(),
+        "did": "device_001",
+        "dimensions": classContext,
+        "edata": {
+            "value": 100
+        }
+    }, function(error, response, body) {
+        if (error) console.log(error);
+        console.log(body);
+        res.status(200).send({ id: packetId });
+    })
 });
 
 app.delete("/packet", function(req, res) {
-  packetId = undefined;
-  res.status(200).send({ deleted: true });
+    packetId = undefined;
+    res.status(200).send({ deleted: true });
 });
 
-const request = require("request-promise"); //  'request' npm package with Promise support
-const getQuestions = async allQuestionIds => {
-  const questions = allQuestionIds.map(async questionId => {
-    const options = {
-      method: "GET",
-      url: `https://dev.sunbirded.org/action/assessment/v3/items/read/${questionId}`,
-      json: true
-    };
-    const response = await request(options);
-    return response.result.assessment_item;
-  });
-  return await Promise.all(questions);
-};
-app.use("/get/:id", async (req, res) => {
-  const options = {
-    method: "GET",
-    url: `https://dev.sunbirded.org/api/content/v1/read/${
-      req.params.id
-    }?fields=questions`,
-    json: true
-  };
-  let allQuestions;
-  try {
-    allQuestions = await request(options);
-    const allQuestionIds = [];
-    allQuestions.result.content.questions.forEach(question => {
-      allQuestionIds.push(question.identifier);
+app.post("/classroom/telemetry", function(req, res) {
+    var events = req.body.events;
+    var interactions = []
+    events.forEach(function(event) {
+        var topic = _.find(event.context.cdata, _.matchesProperty('type', 'topic'));
+        if(topic) {
+            var topicId = topic[0].id;
+            if(!topicData[topicId]) topicData[topicId] = {eventsCount: 0, score: 0, maxscore: 0};
+            topicData[topicId].eventsCount = topicData[topicId].eventsCount + 1;
+            if(event.eid === 'ASSESS') {
+                topicData[topicId].score = topicData[topicId].score + event.edata.score;
+                topicData[topicId].maxscore = topicData[topicId].score + event.edata.maxscore;
+            }    
+        }
+        
+        interactions.push({
+            "eid": "DC_INTERACT",
+            "ets": event.ets,
+            "did": event.context.did,
+            "dimensions": _.assign(classContext, {topics: [topicId]}),
+            "edata": {}
+        })
     });
-    questionMeta = await getQuestions(allQuestionIds);
-    res.json(questionMeta);
-  } catch (error) {
-    console.log(error);
-    if (error.error) {
-      res.status(404).json(error.error);
-    } else {
-      res.status(404).json("unhandled erro");
-    }
-  }
+    postTelemetryEvents(events, function(error, response, body) {
+        if (error) console.log(error);
+        console.log(body);
+        res.status(200).send({});
+    });
 });
+
+app.get("/classroom/end", function(req, res) {
+
+    var events = [];
+    _.keysIn(topicData).forEach(function(key) {
+        var data = topicData[key];
+        events.push({
+            "eid": "DC_ENGAGEMENT",
+            "ets": new Date().getTime(),
+            "did": "device_001",
+            "dimensions": _.assign(classContext, {topics: [key]}),
+            "edata": {
+                "value": data.eventsCount > 100 ? 100 : data.eventsCount
+            }
+        });
+        events.push({
+            "eid": "DC_PERFORMANCE",
+            "ets": new Date().getTime(),
+            "did": "device_001",
+            "dimensions": _.assign(classContext, {topics: [key]}),
+            "edata": {
+                "value": _.ceil((data.score * 100)/ data.maxscore)
+            }
+        });
+    });
+    postTelemetryEvents(events, function(error, response, body) {
+        if (error) console.log(error);
+        console.log(body);
+        res.status(200).send({});
+    })
+});
+
+app.use("/get/:id", async (req, res) => {
+    const options = {
+        method: "GET",
+        url: `https://dev.sunbirded.org/api/content/v1/read/${req.params.id}?fields=questions`,
+        json: true
+    };
+    let allQuestions;
+    try {
+        allQuestions = await request(options);
+        const allQuestionIds = [];
+        allQuestions.result.content.questions.forEach(question => {
+            allQuestionIds.push(question.identifier);
+        });
+        questionMeta = await getQuestions(allQuestionIds);
+        res.json(questionMeta);
+    } catch (error) {
+        console.log(error);
+        if (error.error) {
+            res.status(404).json(error.error);
+        } else {
+            res.status(404).json("unhandled erro");
+        }
+    }
+});
+
+//
+//  Classroom APIs - End
+//
+
+function postTelemetryEvent(event, cb) {
+    postTelemetryEvents([event], cb);
+}
+
+function postTelemetryEvents(events, cb) {
+    var options = {
+        method: "POST",
+        url: "http://52.172.188.118:3000/v1/telemetry",
+        headers: {
+            "Postman-Token": "8201b334-e622-4920-b760-6c4ab83ad26d",
+            "cache-control": "no-cache",
+            "Content-Type": "application/json"
+        },
+        body: { events: events },
+        json: true
+    };
+
+    request1(options, cb);
+}
 
 app.post("/telemetry", function(req, res) {
-  console.log(res.body);
-  var options = {
-    method: "POST",
-    url: "http://52.172.188.118:3000/v1/telemetry",
-    headers: {
-      "Postman-Token": "8201b334-e622-4920-b760-6c4ab83ad26d",
-      "cache-control": "no-cache",
-      "Content-Type": "application/json"
-    },
-    body: req.body,
-    json: true
-  };
 
-  request1(options, function(error, response, body) {
-    if (error) console.log(error);
-    res.status(200).send(body);
-    console.log(body);
-  });
+    postTelemetryEvents(req.body, function(error, response, body) {
+        if (error) console.log(error);
+        console.log(body);
+        res.status(200).send(body);
+    });
 });
 
 var port = process.env.PORT || 3000;
 
 http.listen(port, function() {
-  console.log("listening in http://localhost:" + port);
+    console.log("listening in http://localhost:" + port);
 });
